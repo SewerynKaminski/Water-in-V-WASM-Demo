d5529fd40e50a4942f00f6e20fe00f3487ee5759
SewerynKaminski <Seweryn.kaminski@gmail.com>
2024-08-04 12:33:10 +0200

    formmating

--- water.wasm.v
+++ water.wasm.v
@@ -46,43 +46,43 @@ fn srand ( seed u32 ) { // set seed
 }
 
 pub fn (mut this App) draw () {
-	mut i:= i32(0)
+  mut i:= i32(0)
     
   i = this.old_idx
   this.old_idx = this.new_idx
   this.new_idx = i
-	i=0
-	// Initialize the looping values - each will be incremented
+  i=0
+  // Initialize the looping values - each will be incremented
   mut map_idx := this.old_idx
 
   for y:=0; y < height; y+=1 {
-		map_idx+=1
-	  i+=1
+    map_idx+=1
+    i+=1
     for x:=1; x < width-1; x+=1 {
-		    // Use ripple_map to set data value, map_idx = oldIdx
-		    // Use averaged values of pixels: above, below, left and right of current	
-		    mut data:= this.ripple_map [map_idx - 1]
-		    data += this.ripple_map [map_idx + 1]
-		    data += this.ripple_map [map_idx - width]
-		    data += this.ripple_map [map_idx + width]			
+        // Use ripple_map to set data value, map_idx = oldIdx
+        // Use averaged values of pixels: above, below, left and right of current	
+        mut data:= this.ripple_map [map_idx - 1]
+        data += this.ripple_map [map_idx + 1]
+        data += this.ripple_map [map_idx - width]
+        data += this.ripple_map [map_idx + width]			
         data >>= 1    // right shift 1 is same as divide by 2
-		    // Subtract 'previous' value (we are about to overwrite rippleMap[newIdx+i])
-		    data -= this.ripple_map[this.new_idx + i]
-		    
-		    // Reduce value more -- for damping
-		    // data = data - (data / 32)
+        // Subtract 'previous' value (we are about to overwrite rippleMap[newIdx+i])
+        data -= this.ripple_map[this.new_idx + i]
+
+        // Reduce value more -- for damping
+        // data = data - (data / 32)
         data -= data >> 5
-		    
-		    // Set new value
-		    ii := this.new_idx + i
-		    this.ripple_map[ii] = data
+
+        // Set new value
+        ii := this.new_idx + i
+        this.ripple_map[ii] = data
 
         // If data = 0 then water is flat/still,
-		    // If data != 0 then water has a wave
+        // If data != 0 then water has a wave
         data = (1<<10) - data  // data > 0
         
-		    old_data := this.last_map[i]
-		    this.last_map[i] = data
+        old_data := this.last_map[i]
+        this.last_map[i] = data
         
         if old_data != data { // if no change no need to alter image
             // Calculate pixel offsets
@@ -94,15 +94,15 @@ pub fn (mut this App) draw () {
             if a < 0 { a = 0 }
             if b >= height { b = height - 1 }
             if b < 0 { b = 0 }
-			
-			      // Apply values
-			      this.frame[i] = this.texture[a + (b * width)]
+
+            // Apply values
+            this.frame[i] = this.texture[a + (b * width)]
         }
         map_idx += 1
         i += 1
     }
-		map_idx+=1
-		i+=1
+    map_idx+=1
+    i+=1
   }
 }
 
@@ -118,15 +118,15 @@ pub fn (mut this App)mousemove ( x u32, y u32 ) {
     ymr := i32(y - ripple_r)
     xpr := x + ripple_r
     ypr := y + ripple_r
-	
+
     for j:= ymr; j < ypr; j+=1 {
         if j<0 { j=0 }
-	      if j>=height { break }
+        if j>=height { break }
         for k:=xmr; k < xpr; k+=1 {	    
-	          if k<1 { k=1 }
-	          if k>=width-1 { break }
-	          i:= this.old_idx + ((j * i32(width)) + k)
-	          this.ripple_map[i] += 512
+            if k<1 { k=1 }
+            if k>=width-1 { break }
+            i:= this.old_idx + ((j * i32(width)) + k)
+            this.ripple_map[i] += 512
         }
     }
 }
@@ -140,11 +140,11 @@ pub fn (mut app App)keydown ( key &u8, code &u8 ) {}
 fn cmp ( a &u8, b &u8 ) bool {
     unsafe {
         for *a > 0 && *b > 0 {
-		        if *a != *b { break }
-		        a += 1
-		        b += 1
+            if *a != *b { break }
+            a += 1
+            b += 1
         }
-	      return *a == *b
+        return *a == *b
     }
 }
 
